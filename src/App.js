d34a99e0e2d05e9b383407605724c941ab16a262
Raynor Kuang <raynorkuang@gmail.com>
2019-10-17 13:47:45 -0400

    add state / hook / supporting elements

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
 import './App.css';
 
-const Button = ({ text, background }) => {
+const Button = ({ text, background, onClick, currentText, setText }) => {
   const style = {};
 
   if (background) {
     style.background = background;
   }
 
+  const handleClick = onClick || (() => setText(currentText + " " + text));
+
   return (
-    <button className="button" style={style}>
+    <button className="button" style={style} onClick={handleClick}>
       I am a {text} button
     </button>
   );
@@ -20,15 +22,25 @@ const Button = ({ text, background }) => {
 Button.propTypes = {
   text: PropTypes.string.isRequired,
   background: PropTypes.string,
+  onClick: PropTypes.func,
+  currentText: PropTypes.string,
+  setText: PropTypes.func,
 };
 
 function App() {
+  const [text, setText] = useState("");
+
   return (
     <div className="App">
-      <Button text="one" />
-      <Button text="two" />
-      <Button text="red" background="red" />
-      <Button text="blue" background="blue" />
+      <Button text="one" onClick={() => setText(text + " one")} />
+      <Button text="two" onClick={() => setText(text + " two two")} />
+      <Button text="red" background="red" currentText={text} setText={setText} />
+      <Button text="blue" background="blue" currentText={text} setText={setText} />
+      <Button text="reset" onClick={() => setText("")} />
+
+      <div className="text">
+        { text || "Click a button to add text!" }
+      </div>
     </div>
   );
 }
